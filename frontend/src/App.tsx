d2ace682e7005e781b220cbe42b23d0d5c9a1db1
Martina-Garcia <182167292+Martina-Garcia@users.noreply.github.com>
2025-09-14 19:58:43 -0300

    Logos agregados
    Se agregan los logos de la escuela de ingenieria y de la ucn a la pantalla de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 // src/App.tsx
+
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
+import logoUcn from "./recursos/logo-ucn.png";
+import escIngeniria from "./recursos/escuela-ingenieria.png";
+
+
 
 export default function App() {
   return (
@@ -22,6 +27,8 @@ export default function App() {
 function Login() {
   return (
     <div>
+      <img src={logoUcn} className="logo" alt="Logo UCN" />
+      <img src={escIngeniria} className="logo" alt="Escuela de Ingeniería" />
       <h2>¡Bienvenido!</h2>
       <p>Inicie Sesión para comenzar a crear sus proyecciones</p>
     </div>
