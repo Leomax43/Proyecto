26d5317fc293a3ff208eb8258d6dc42f5c86cfb8
Leomax43 <130073563+Leomax43@users.noreply.github.com>
2025-10-20 22:05:57 -0300

    docker 1

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UcnController } from './ucn/ucn.controller';
@@ -7,7 +8,18 @@ import { UcnService } from './ucn/ucn.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ucn_user',
+      password: 'ucn_pass',
+      database: 'proyecciones_db',
+      autoLoadEntities: true,
+      synchronize: true, // ⚠️ solo en desarrollo
+    }),
+  ],
   controllers: [AppController, UsersController, UcnController],
   providers: [AppService, UcnService],
 })
