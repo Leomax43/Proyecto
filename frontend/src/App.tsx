e2e96e1fbbaa397bf7173244aa2cff9e520d3c81
lavaca777 <107744299+lavaca777@users.noreply.github.com>
2025-12-11 02:19:41 -0300

    sidebar definitivo
    se agregaron iconos a los botones, como tambien un boton de minimizar, para una vista mas amplia de la pagina

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+/*
 import { Routes, Route, Link } from "react-router-dom";
 
 import HomeScreen from "./HomeScreen";
@@ -12,6 +13,32 @@ export default function App() {
       <nav>
         <Link to="/">Login</Link> | <Link to="/home">Home</Link> | <Link to="/malla">Malla</Link>
       </nav>
+
+      {}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/malla" element={<Malla />} />
+        <Route path="/avance" element={<Avance />} />
+        <Route path="/proyecciones" element={<Proyecciones />} />
+      </Routes>
+    </div>
+    
+  );
+}
+*/
+import { Routes, Route } from "react-router-dom";
+
+import HomeScreen from "./HomeScreen";
+import Login from "./components/Login";
+import Malla from "./Malla"; 
+import Avance from "./Avance";
+import Proyecciones from "./Proyecciones";
+
+export default function App() {
+  return (
+    <div className="App">
+
 
       {}
       <Routes>
